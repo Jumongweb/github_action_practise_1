a2f7d5fbb559bd3dd3df0649aedfd20943d84c42
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-10-20 13:26:33 +0100

    first commit

---
+++ src/main/java/com/example/github_action_practise_1/GithubActionPractise1Application.java
@@ -0,0 +1,13 @@
+package com.example.github_action_practise_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionPractise1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionPractise1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/github_action_practise_1/GithubActionPractise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_action_practise_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionPractise1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
